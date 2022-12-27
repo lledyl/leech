f189a403356af83184b0c3a47afc8f5a2c0c8817
lledyl <53127047+lledyl@users.noreply.github.com>
2022-12-26 19:49:37 -0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MM=10
+MM=12
 tput setaf 2; echo "Checking duplicate process"
 PIDFILE=~/.lock_ju.pid
 if [ -f $PIDFILE ]
@@ -32,14 +32,14 @@ fi
 tput setaf 2; echo "No duplicate process found"
 tput setaf 2; echo "Moving files to Google drive"
 tput setaf 7;
-rclone move complete/ suke1:2023/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.suke1.log --drive-stop-on-upload-limit
-rclone move complete/ suke2:2023/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.suke2.log --drive-stop-on-upload-limit
-rclone move complete/ suke3:2023/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.suke3.log --drive-stop-on-upload-limit
-rclone move complete/ suke4:2023/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.suke4.log --drive-stop-on-upload-limit
+rclone move complete/ suke1:2022/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.suke1.log --drive-stop-on-upload-limit
+rclone move complete/ suke2:2022/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.suke2.log --drive-stop-on-upload-limit
+rclone move complete/ suke3:2022/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.suke3.log --drive-stop-on-upload-limit
+rclone move complete/ suke4:2022/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.suke4.log --drive-stop-on-upload-limit
 
-rclone move complete/ rar1:2023/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.rar1.log --drive-stop-on-upload-limit
-rclone move complete/ rar2:2023/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.rar2.log --drive-stop-on-upload-limit
-rclone move complete/ rar3:2023/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.rar3.log --drive-stop-on-upload-limit
+rclone move complete/ rar1:2022/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.rar1.log --drive-stop-on-upload-limit
+rclone move complete/ rar2:2022/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.rar2.log --drive-stop-on-upload-limit
+rclone move complete/ rar3:2022/2022-$MM/  --transfers 2  --min-age 61s --include-from .filter.txt --size-only --delete-empty-src-dirs   --log-file=.rar3.log --drive-stop-on-upload-limit
 tput setaf 2; echo "Delete lock file"
 rm $PIDFILE
 echo "Done"
