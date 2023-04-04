6ab351abe910fb729001a7c597d5a7f4fda9e1b8
lledyl <53127047+lledyl@users.noreply.github.com>
2023-04-03 20:14:31 -0600

    Update and rename transmission_check.sh to check_transmission.sh

--- check_transmission.sh
+++ check_transmission.sh
@@ -7,5 +7,5 @@ else
     echo "$SERVICE stopped and now restarting"
     sudo service transmission-daemon restart
     sleep 5
-    sh /home/vm2k23q2/clean_transmission.sh
+    sh /home/vm2k23q2/check_transmission.sh
 fi
