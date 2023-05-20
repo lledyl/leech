29a91662a6c9095d0cc1dc1caecc525eb9856e47
lledyl <53127047+lledyl@users.noreply.github.com>
2023-05-19 18:24:16 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ cd $home
 sudo apt-get -y install transmission-cli  transmission-daemon
 sudo service transmission-daemon stop
 curl -s https://raw.githubusercontent.com/lledyl/leech/main/settings.json --output settings.json
-curl -s https://raw.githubusercontent.com/lledyl/lecch/main/upload.sh --output upload.sh
+curl -s https://raw.githubusercontent.com/lledyl/leech/main/upload.sh --output upload.sh
 curl -s https://raw.githubusercontent.com/lledyl/leech/main/filter.txt --output .filter.txt
 sudo mv settings.json /etc/transmission-daemon/settings.json
 sudo ln -s /etc/transmission-daemon/settings.json /home/$USER/settings
