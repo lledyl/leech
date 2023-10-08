f31247912e8a663a2a3259bde656f3b09ff2f9f4
lledyl <53127047+lledyl@users.noreply.github.com>
2023-10-08 12:52:00 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,23 +20,6 @@ sudo apt-get -y install nano
 sudo apt-get -y install bmon
 sudo apt-get -y install screen
 sudo apt-get -y install cron
-sudo apt-get -y install rename
-sudo apt-get -y install python3-venv
-
-python3 -m venv ~/flexget/
-cd ~/flexget/
-bin/pip install flexget
-source ~/flexget/bin/activate
-pip install transmission-rpc -U
-cd $home
-
-curl https://rclone.org/install.sh | sudo bash
-wget https://raw.githubusercontent.com/lledyl/Public/master/config.yml -O config.yml
-wget https://raw.githubusercontent.com/lledyl/Public/master/helpcommands.txt -O help.txt
-crontab -l > mycron
-echo "#*/30 * * * * ~/flexget/bin/flexget execute" >> mycron
-crontab mycron
-rm mycron
 
 
 
@@ -86,9 +69,4 @@ rm mycron
 cd $home
 wget https://gist.githubusercontent.com/pawelszydlo/e2e1fc424f2c9d306f3a/raw/c26087d4b4f696bd373b02c0e294fb92dec1039a/transmission_remove_finished.sh -O transmission_remove_finished.sh
 mv transmission_remove_finished.sh clean_transmission.sh
-cd /usr/share/transmission/
-sudo wget https://github.com/Secretmapper/combustion/archive/release.zip -O release.zip
-sudo unzip -o release.zip
-sudo mv web web_orig
-sudo mv combustion-release/ web
-sudo rm release.zip
+
