30233b8d411b55c589144ff577c4ab023c3896c3
lledyl <53127047+lledyl@users.noreply.github.com>
2023-11-28 15:02:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,20 +17,17 @@ sudo ln -s /etc/transmission-daemon/settings.json /home/$USER/settings
 sudo mkdir -p /content/downloads/
 sudo chown $USER:$USER /content/downloads/
 sudo chmod 755 /content/downloads/
-sudo mkdir /content/downloads/c
-sudo mkdir /content/downloads/i
+sudo mkdir /content/downloads/complete
+sudo mkdir /content/downloads/incomplete
 
-sudo chmod 775 /content/downloads/c
-sudo chmod 775 /content/downloads/i
+sudo chmod 775 /content/downloads/complete
+sudo chmod 775 /content/downloads/incomplete
 sudo usermod -a -G debian-transmission $USER
 sudo adduser $USER debian-transmission
 
 
-sudo chown -R $USER:debian-transmission /content/downloads/c
-sudo chown -R $USER:debian-transmission /content/downloads/i
-
-sudo ln -s /content/downloads/c /home/$USER/complete
-sudo ln -s /content/downloads/i /home/$USER/incomplete
+sudo chown -R $USER:debian-transmission /content/downloads/complete
+sudo chown -R $USER:debian-transmission /content/downloads/incomplete
 
 cd /usr/share/transmission/
 sudo wget https://github.com/Secretmapper/combustion/archive/release.zip -O release.zip
