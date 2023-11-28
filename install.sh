fb2af0bdbd9ac60a5adbbc6bdf3bb4512ce32241
lledyl <53127047+lledyl@users.noreply.github.com>
2023-11-28 15:07:06 -0700

    Update install.sh
    sudo mkdir -p /content/downloads/
    sudo chown $USER:$USER /content/downloads/
    sudo chmod 755 /content/downloads/
    sudo mkdir /content/downloads/complete
    sudo mkdir /content/downloads/incomplete
    
    sudo chmod 775 /content/downloads/complete
    sudo chmod 775 /content/downloads/incomplete
    sudo usermod -a -G debian-transmission $USER
    sudo adduser $USER debian-transmission
    
    
    sudo chown -R $USER:debian-transmission /content/downloads/complete
    sudo chown -R $USER:debian-transmission /content/downloads/incomplete

--- install.sh
+++ install.sh
@@ -14,20 +14,7 @@ curl -s https://raw.githubusercontent.com/lledyl/leech/main/filter.txt --output 
 sudo mv settings.json /etc/transmission-daemon/settings.json
 sudo ln -s /etc/transmission-daemon/settings.json /home/$USER/settings
 
-sudo mkdir -p /content/downloads/
-sudo chown $USER:$USER /content/downloads/
-sudo chmod 755 /content/downloads/
-sudo mkdir /content/downloads/complete
-sudo mkdir /content/downloads/incomplete
 
-sudo chmod 775 /content/downloads/complete
-sudo chmod 775 /content/downloads/incomplete
-sudo usermod -a -G debian-transmission $USER
-sudo adduser $USER debian-transmission
-
-
-sudo chown -R $USER:debian-transmission /content/downloads/complete
-sudo chown -R $USER:debian-transmission /content/downloads/incomplete
 
 cd /usr/share/transmission/
 sudo wget https://github.com/Secretmapper/combustion/archive/release.zip -O release.zip
