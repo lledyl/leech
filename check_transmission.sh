d51f9af42fedd51ba030a09e158f573dc9881746
lledyl <53127047+lledyl@users.noreply.github.com>
2023-12-01 11:22:10 -0700

    Delete check_transmission.sh

--- check_transmission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SERVICE="transmission-daemon"
-if pidof -x "$SERVICE" > /dev/null
-then
-    echo "$SERVICE is running"
-else
-    echo "$SERVICE stopped and now restarting"
-    sudo service transmission-daemon restart
-fi
