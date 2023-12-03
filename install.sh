9e5d251604cae5dbccefe4437f537037ce4be2f4
lledyl <53127047+lledyl@users.noreply.github.com>
2023-12-03 10:52:48 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash/
 
-
-sudo apt-get update
-sudo apt-get -y install wget
-sudo apt-get -y install unzip
-
+echo "Installing Requirements"
+#sudo apt-get update -y &> /dev/null
+sudo apt-get -y install wget -y &> /dev/null
+sudo apt-get -y install unzip -y &> /dev/null
+echo "Installing Transmission"
 cd $home
-sudo apt-get -y install transmission-cli  transmission-daemon
+sudo apt-get -y install transmission-cli  transmission-daemon -y &> /dev/null
 sudo service transmission-daemon stop
 curl -s https://raw.githubusercontent.com/lledyl/leech/main/settings.json --output settings.json
 sudo mv settings.json /etc/transmission-daemon/settings.json
 sudo ln -s /etc/transmission-daemon/settings.json /home/$USER/settings
-
+echo "Setting up Folders"
 
 sudo mkdir -p /content/downloads/
 sudo chown root:root /content/downloads/
@@ -31,10 +31,11 @@ sudo sed -i 's/USER=debian-transmission/USER=root/g' /etc/init.d/transmission-da
 sleep 5
 sudo service transmission-daemon start
 
-
+echo "Installing Combustion"
 cd /usr/share/transmission/
 sudo wget https://github.com/Secretmapper/combustion/archive/release.zip -O release.zip
 sudo unzip -o release.zip
 sudo mv web web_orig
 sudo mv combustion-release/ web
 sudo rm release.zip
+echo "Done!"
